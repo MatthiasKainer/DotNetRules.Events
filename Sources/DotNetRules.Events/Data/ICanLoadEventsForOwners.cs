ab59f872ace26fcf9a4452578c4617358060adab
Matthias Kainer <mkainer@autoscout24.com>
2014-09-05 08:49:29 +0200

    Added interface to allow loading of events for owners

---
+++ Sources/DotNetRules.Events/Data/ICanLoadEventsForOwners.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetRules.Events.Data
+{
+    using System.Collections.Generic;
+
+    public interface ICanLoadEventsForOwners
+    {
+        IEnumerable<BaseEvent> GetAllEvents(string forApplication);
+
+        IEnumerable<TEvents> GetSpecificEventsFromStore<TEvents>(string forApplication, string owner) where TEvents : BaseEvent;
+
+        IEnumerable<TEvents> GetEventsFromStoreByEntity<TEvents>(string forApplication, string owner) where TEvents : BaseEvent;
+    }
+}
